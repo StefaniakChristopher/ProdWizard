2b3e88c656dcaec28f651d82b25b48b2d53e7ae4
Jacob Chunn <chunnjake@gmail.com>
2023-10-04 10:35:25 -0400

    Add index

---
+++ frontend/myScript.js
@@ -0,0 +1,15 @@
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+
+console.log("hello!");
+let host = "http://localhost:8080";
+httpGetAsync(host);
